99eebf8c25e0557b4e11c3928ca64810186bafef
Siphamandla Sisa Sidelo <sidelosisa@gmail.com>
2024-08-14 06:47:20 +0200

    minor changes

--- src/app/components/Welcome.js
+++ src/app/components/Welcome.js
@@ -1,4 +1,3 @@
-// components/Welcome.js
 import React from "react";
 import Link from 'next/link';
 
@@ -8,14 +7,14 @@ export default function Welcome() {
       <div className="container mx-auto flex px-1 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Sisa.
+            Hi, I&apos;m Sisa.
             <br className="hidden lg:inline-block" />SQL Developer. Reporting and Analytics Specialist. Database Administrator
           </h1>
           <p className="mb-8 leading-relaxed">
                 As a dedicated SQL Developer with a focus on T-SQL, 
                 I bring three years of experience in database administration, reporting, and analytics to the table. 
                 My expertise spans designing and optimizing complex queries, managing high-profile projects, and delivering actionable insights through advanced reporting solutions. 
-                I've successfully optimised the Gauteng Online Admissions Placement Engine, which significantly improved its performance by 71%. 
+                I&apos;ve successfully optimised the Gauteng Online Admissions Placement Engine, which significantly improved its performance by 71%. 
                 I thrive in dynamic environments, leveraging my technical skills and project experience to drive data-driven decision-making and enhance operational efficiency.
           </p>
           <div className="flex justify-center">
